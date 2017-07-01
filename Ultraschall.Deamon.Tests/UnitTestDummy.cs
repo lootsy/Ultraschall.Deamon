512f74723696348ada1bf780d897904a80f9434a
Daniel Lindenfelser <daniel@lindenfelser.de>
2017-07-02 00:05:57 +0200

    add unit test project

---
+++ Ultraschall.Deamon.Tests/UnitTestDummy.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ultraschall.Deamon.Tests
+{
+    [TestClass]
+    public class UnitTestDummy
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
